e3e74488242c0f59e5574d111c6f6c1a4ecdfba9
Jonathan Cadena <jonathancadenaloaiza@gmail.com>
2026-05-06 16:09:43 -0500

    Add env.js with group rosters configuration

---
+++ js/env.js
@@ -0,0 +1,26 @@
+window.ENV = window.ENV || {};
+window.ENV.GROUP_ROSTERS = {
+    1: [
+        { name: 'Adames Tobon, Arcenio', role: 'Director General' },
+        { name: 'Franco Rincon, Daniel Felipe', role: 'Director Legal' },
+        { name: 'García Torres, Jorge Iván', role: 'Director Ambiental' }
+    ],
+    2: [
+        { name: 'Benitez Molina, Alejandro', role: 'Director General' },
+        { name: 'Garcia Hincapie, Juan Sebastian', role: 'Director Legal' },
+        { name: 'Serna Cardona, Jhonier', role: 'Director Ambiental' }
+    ],
+    3: [
+        { name: 'Cano Buitrago, Paula Andrea', role: 'Director General' }
+    ],
+    5: [
+        { name: 'Castillo Galeano, Nicolas', role: 'Director General' },
+        { name: 'Silva Guarnizo, Santiago', role: 'Director Legal' },
+        { name: 'Montanchez Botina, Juan Felipe', role: 'Director Ambiental' }
+    ],
+    6: [
+        { name: 'Martínez Henao, Andrés Felipe', role: 'Director General' },
+        { name: 'Usma Londoño, Liceth Juanita', role: 'Director Legal' },
+        { name: 'Buitrago Alzate, Vanesa', role: 'Director Ambiental' }
+    ]
+};
